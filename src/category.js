c045365e0516604b08e51f57ea187ddced8cb5af
musiccity365 <m_sackstein@yahoo.com>
2021-05-02 23:44:58 -0400

    --- Auto-Git Commit ---

--- src/category.js
+++ src/category.js
@@ -1,12 +1,12 @@
 // NEW
 
-class Category{
+class Category {
     static all = []
 
     static categoryContainer = document.getElementById('cat-container')
 
-    constructor({id, name }){
-        this.id = id 
+    constructor({ id, name }) {
+        this.id = id
         this.name = name
         this.active = false
 
@@ -15,49 +15,49 @@ class Category{
         Category.all.push(this)
     }
 
-    items(){
+    items() {
         return Item.all.filter((item) => item.categoryId === parseInt(this.id))
     }
 
 
-    render(){
-        this.element.innerText = this.name 
+    render() {
+        this.element.innerText = this.name
         this.element.id = `category-${this.id}`
         return this.element
     }
 
-    addToDom(){
+    addToDom() {
         Category.categoryContainer.append(this.render())
         this.addListeners()
     }
 
-    addListeners(){
+    addListeners() {
         this.element.addEventListener('click', this.setActiveCategory)
     }
 
     setActiveCategory = (e) => {
-        let filteredCategory 
+        let filteredCategory
         Category.all.forEach(c => {
-    
-            if(c.element === this.element && !this.active){
-                
+
+            if (c.element === this.element && !this.active) {
+
                 c.element.classList.add('activated')
                 c.active = true
                 filteredCategory = c
-            } else{
+            } else {
                 c.element.classList.remove('activated')
                 c.active = false
             }
-            
-        }) 
-        
+
+        })
+
         Item.filterByCategory(filteredCategory)
     }
 
 
-    addToDropDown(){
+    addToDropDown() {
         const option = document.createElement('option')
-        option.value  = this.id 
+        option.value = this.id
         option.innerText = this.name
         dropdown.append(option)
     }
