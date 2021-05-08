c80c89df9ecb2562954ff091424f84e6a1ee7d08
musiccity365 <m_sackstein@yahoo.com>
2021-05-07 22:42:45 -0400

    update bundle gems

--- src/categoryApi.js
+++ src/categoryApi.js
@@ -3,17 +3,17 @@
 class CategoryApi {
 
     static baseURL = 'http://localhost:3000/categories'
-        
 
-    static getCategories(){
+
+    static getCategories() {
         fetch(this.baseURL)
-        .then(r => r.json())
-        .then( json => {
-            json["data"].forEach(element => {
-                const c = new Category({id: element.id, ...element.attributes})
-                c.addToDom()
-                c.addToDropDown()
+            .then(r => r.json())
+            .then(json => {
+                json["data"].forEach(element => {
+                    const c = new Category({ id: element.id, ...element.attributes })
+                    c.addToDom()
+                    c.addToDropDown()
+                })
             })
-        })
     }
 }
